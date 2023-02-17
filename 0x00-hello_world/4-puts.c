f76166a76dd0749b4d944a8efb4b1b31ba8500b9
Olumide <olumyd07@gmail.com>
2023-02-16 20:39:32 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main -  prints "Programming is like building a multilingual puzzle," followed by a new line
+ * main -  prints with puts function, followed by a new line
  * Return: Always 0.
  */
 int main(void)
