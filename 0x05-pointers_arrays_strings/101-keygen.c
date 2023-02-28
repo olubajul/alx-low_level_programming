f12f8e69647d7c572b61941b7a6e009ac559dbbe
Olumide <olumyd07@gmail.com>
2023-02-27 17:01:30 -0800

    Generating random valid password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -34,11 +34,13 @@ int main(void)
 	}
 	}
 	for (index = 0; password(index; index++)
-	}
+	{
 	if (password[index] >= (33 + diff_half2))
 	{
 	password[index] -= diff_half2;
 	break;
+	}
+	}
 	}
 	printf("%s", password);
 	return (0);
