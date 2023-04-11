d6b6425850e6103e5368ea55a088d290d32625d9
Olumide <olumyd07@gmail.com>
2023-04-10 21:39:11 -0700

    Elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -170,6 +170,119 @@ void print_osabi(unsigned char *e_ident)
 	}
 }
 
+/**
+ * print_abi - prints the ABI version of an ELF header
+ * @e_ident: pointer to an array containing the ELF ABI version
+ */
+void print_abi(unsigned char *e_ident)
+{
+	printf(" ABI Version:                            %d\n",
+			e_ident[EI_ABIVERSION]);
+}
+
+/**
+ * print_type - prints the type of an ELF header
+ * @e_type: the ELF type
+ * @e_ident: pointer to an array containing the ELF class
+ */
+void print_type(unsigned int e_type, unsigned char *e_ident)
+{
+	if (e_ident[EI_DATA] == ELFDATA2MSB)
+		e_type >>= 8;
+	printf("   Type:                              ");
+	switch (e_type)
+	{
+		case ET_NONE:
+			printf("NONE (None)\n");
+			break;
+		case ET_REL:
+			printf("REL (Relocatable file)\n");
+			break;
+		case ET_EXEC:
+			printf("EXEC (Executable file)\n");
+			break;
+		case ET_DYN:
+			printf("DYN (Shared object file)\n");
+			break;
+		case ET_CORE:
+			printf("CORE (Core file)\n");
+			break;
+		default:
+			printf("<unknown: %x>\n", e_type);
+	}
+}
+
+/**
+ * print_entry - prints the entry point of an ELF header
+ * @e_entry: address of the ELF entry point
+ * @e_ident: pointer to an array containing the ELF class
+ * Description: If the file is not an ELF file, or on error,
+ * exit with status code 98
+ */
+void print_entry(unsigned long int e_entry, unsigned char *e_ident)
+{
+	printf("  Entry point address:                 ");
+	if (e_ident[EI_DATA] == ELFDATA2MSB)
+	{
+		e_entry = ((e_entry << 8) & 0xFF00FF00) | ((e_entry >> 8) & 0xFF00FF);
+		e_entry = (e_entry << 16) | (e_entry >> 16);
+	}
+	if (e_ident[EI_CLASS] == ELFCLASS32)
+		printf("%#x\n", (unsigned int)e_entry);
+	else
+		printf("%#1x\n", (unsigned int)e_entry);
+}
+
+/**
+ * close_elf - closes an Elf file
+ * @elf: file descriptor of the  ELF file
+ * Description: if the file cannot be closed - exit code 98
+ */
+void close_elf(int elf)
+{
+	if (close(elf) == -1)
+	{
+		dprintf(STDERR_FILENO,
+				"Error: Can't close fd %d\n", elf);
+		exit(98);
+	}
+}
+
+/**
+ * main - displays the information contained in the ELF
+ * header at the start of an ELF file
+ * @argc: number of arguments supplied to the program
+ * @argv: array of pointers to the arguments
+ * Return: 0 on success
+ * Description: if the file is not an ELF file or the
+ * function fails - exit code 98
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	Elf64_Ehdr *header;
+	int o, r;
+
+	o = open(argv[1], O_RDONLY);
+	if (o == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+	header = malloc(sizeof(Elf64_Ehdr));
+	if (header == NULL)
+	{
+		close_elf(o);
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+	r = read(o, header, sizeof(Elf64_Ehdr));
+	if (r == -1)
+	{
+		free(header);
+		close_elf(o);
+		dprintf(STDERR_FILENO, "Error: '%s': No such file\n", argv[1]);
+		exit(98);
+	}
 	check_elf(header->e_ident);
 	printf("ELF Header:\n");
 	print_magic(header->e_ident);
